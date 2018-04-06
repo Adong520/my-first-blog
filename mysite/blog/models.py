618da5aff4553d0a8dc7d8395c9170664114eed6
Adong520 <a18758@hotmail.com>
2018-04-06 17:24:38 +1000

    Modified templates to display oosts from database

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.publishe_date = timezon.now()
+        self.publishe_date = timezone.now()
         self.save()
 
     def __str__(self):
